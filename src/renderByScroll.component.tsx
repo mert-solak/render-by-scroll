13e09d3b6bc4451fcfbd1c366ace21c84dc9c0ad
mert-solak <68771376+mert-solak@users.noreply.github.com>
2022-11-24 12:34:32 +0300

    condition update

--- src/renderByScroll.component.tsx
+++ src/renderByScroll.component.tsx
@@ -21,12 +21,7 @@ export const RenderByScroll: React.FC<Props> = ({
 
     const rect = elementRef.current.getBoundingClientRect();
 
-    if (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    ) {
+    if (rect.bottom > 0 && rect.top < window.innerHeight && rect.right > 0 && rect.left < window.innerWidth) {
       setShouldRender(true);
     }
   }, [elementRef]);
